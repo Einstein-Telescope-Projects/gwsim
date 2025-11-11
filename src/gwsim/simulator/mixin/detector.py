1e4b07b9ac8e078acc4d5dd1f238a4bdcae6262d
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-11-11 12:03:40 +0100

    fix: Fix the type hint

--- src/gwsim/simulator/mixin/detector.py
+++ src/gwsim/simulator/mixin/detector.py
@@ -2,13 +2,15 @@
 
 from __future__ import annotations
 
+from pathlib import Path
+
 from gwsim.detector.base import Detector
 
 
 class DetectorMixin:  # pylint: disable=too-few-public-methods
     """Mixin class to add detector information to simulators."""
 
-    def __init__(self, detectors: list[str] | None = None, **kwargs):  # pylint: disable=unused-argument
+    def __init__(self, detectors: list[str | Detector] | None = None, **kwargs):  # pylint: disable=unused-argument
         """Initialize the DetectorMixin.
 
         Args:
@@ -28,12 +30,25 @@ class DetectorMixin:  # pylint: disable=too-few-public-methods
         return self._detectors
 
     @detectors.setter
-    def detectors(self, value: list[str] | None) -> None:
+    def detectors(self, value: list[str | Path | Detector] | None) -> None:
+        """Set the list of detectors.
+
+        Args:
+            value (list[str | Path | Detector] | None):
+                List of detector names, config file paths, or Detector instances.
+                If None, no detectors are set.
+        """
         if value is None:
             self._detectors = None
-        else:
-            # Check whether the label is a file name
-            self._detectors = [Detector.get_detector(det) for det in value]
+        if isinstance(value, list):
+            if all(isinstance(det, Detector) for det in value):
+                self._detectors = value
+                return
+            if all(isinstance(det, str) for det in value) or all(isinstance(det, Path) for det in value):
+                self._detectors = [Detector.get_detector(det) for det in value]
+                return
+            raise ValueError("All elements in detectors list must be of the same type: str, Path, or Detector.")
+        raise ValueError("Detectors must be a list of str, Path, or Detector instances, or None.")
 
     @property
     def metadata(self) -> dict:
